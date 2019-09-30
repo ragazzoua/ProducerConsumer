32546019d1340066ce929780aa8f6e68dd71db6a
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-09-30 15:12:54 +0300

    prod cons implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,23 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import static com.company.Main.EOF;
+
 public class Main {
+    public static final String EOF = "EOF";
 
     public static void main(String[] args) {
-        // write your code here
+        List<String> buffer = new ArrayList<>();
+        MyProducer producer = new MyProducer(buffer, ThreadColour.ANSI_BLUE);
+        MyConsumer consumer1 = new MyConsumer(buffer, ThreadColour.ANSI_PURPLE);
+        MyConsumer consumer2 = new MyConsumer(buffer, ThreadColour.ANSI_CYAN);
+
+        new Thread(producer).start();
+        new Thread(consumer1).start();
+        new Thread(consumer2).start();
     }
 }
 
@@ -24,18 +35,48 @@ class MyProducer implements Runnable {
         Random random = new Random();
         String[] nums = {"1", "2", "3", "4", "5"};
 
-        for (String num: nums){
+        for (String num : nums) {
             try {
                 System.out.println(color + "Adding..." + num);
-                buffer.add(num);
-
+                synchronized (buffer) {
+                    buffer.add(num);
+                }
                 Thread.sleep(1000);
-            }catch (InterruptedException e){
+            } catch (InterruptedException e) {
                 System.out.println("Producer was interrupted");
             }
         }
 
         System.out.println(color + "Adding EOF and existing...");
-        buffer.add("EOF");
+        synchronized (buffer) {
+            buffer.add(EOF);
+        }
+    }
+}
+
+class MyConsumer implements Runnable {
+    private List<String> buffer;
+    private String color;
+
+    public MyConsumer(List<String> buffer, String color) {
+        this.buffer = buffer;
+        this.color = color;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (buffer) {
+                if (buffer.isEmpty()) {
+                    continue;
+                }
+                if (buffer.get(0).equals(EOF)) {
+                    System.out.println(color + "Existing");
+                    break;
+                } else {
+                    System.out.println(color + "Removed " + buffer.remove(0));
+                }
+            }
+        }
     }
 }
